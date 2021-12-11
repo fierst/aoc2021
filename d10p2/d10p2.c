328e7e4c0f64555d95d891796363ffd68844dd07
Evan Fierst <evan.fierst@ultronic.com>
2021-12-10 22:37:31 -0600

    Overflows are dumb, d10p2 done

--- d10p2/d10p2.c
+++ d10p2/d10p2.c
@@ -4,10 +4,22 @@
 
 #include "stack.h"
 
-#define STACK_ERROR_PARENTHESES     3
-#define STACK_ERROR_SQUARE_BRACKET  57
-#define STACK_ERROR_CURLY_BRACE     1197
-#define STACK_ERROR_CARET           25137
+#define INITIAL_CAPACITY 128
+
+#define STACK_AUTOCOMPLETE_PARENTHESES      1
+#define STACK_AUTOCOMPLETE_SQUARE_BRACKET   2
+#define STACK_AUTOCOMPLETE_CURLY_BRACE      3
+#define STACK_AUTOCOMPLETE_CARET            4
+
+typedef struct {
+    uint64_t* items;
+    int count;
+    int capacity;
+} vector_t;
+
+void vector_push_back(vector_t * v, uint64_t value);
+
+int vector_sort_comp(const void * a, const void * b);
 
 int main(int argc, char const *argv[])
 {
@@ -15,13 +27,22 @@ int main(int argc, char const *argv[])
     size_t len;
     ssize_t read;
 
+    vector_t stack_scores = {
+        .capacity = INITIAL_CAPACITY,
+        .count = 0,
+        .items = calloc(INITIAL_CAPACITY, sizeof(int))
+    };
+
     stack_t stack;
 
     uint8_t stack_error;
 
-    int stack_error_value = 0;
+    uint64_t stack_score;
     
     while(read = getline(&line, &len, stdin) != EOF) {
+
+        stack_score = 0;
+
         char * p = line;
 
         init_stack(&stack, STACK_DEFAULT_CAPACITY);
@@ -41,7 +62,6 @@ int main(int argc, char const *argv[])
                         pop(&stack);
                     } else {
                         stack_error = 1;
-                        stack_error_value += STACK_ERROR_CURLY_BRACE;
                     }
                     break;
                 case '>':
@@ -49,7 +69,6 @@ int main(int argc, char const *argv[])
                         pop(&stack);
                     } else {
                         stack_error = 1;
-                        stack_error_value += STACK_ERROR_CARET;
                     }
                     break;
                 case ']':
@@ -57,7 +76,6 @@ int main(int argc, char const *argv[])
                         pop(&stack);
                     } else {
                         stack_error = 1;
-                        stack_error_value += STACK_ERROR_SQUARE_BRACKET;
                     }
                     break;
                 case ')':
@@ -65,20 +83,63 @@ int main(int argc, char const *argv[])
                         pop(&stack);
                     } else {
                         stack_error = 1;
-                        stack_error_value += STACK_ERROR_PARENTHESES;
                     }
                     break;
+            }  
+
+            p++;           
+        }
+
+        if(!stack_error) {
+            while(stack.top != 0) {                
+                stack_score *= 5;
+                switch(top(&stack)) {
+                    case '{':
+                        stack_score += STACK_AUTOCOMPLETE_CURLY_BRACE;
+                        break;
+                    case '<':
+                        stack_score += STACK_AUTOCOMPLETE_CARET;
+                        break;
+                    case '[':
+                        stack_score += STACK_AUTOCOMPLETE_SQUARE_BRACKET;
+                        break;
+                    case '(':
+                        stack_score += STACK_AUTOCOMPLETE_PARENTHESES;
+                        break;
+                }
+                pop(&stack);
             }
 
-            if(!stack_error)    p++;                
+            vector_push_back(&stack_scores, stack_score);
         }
     }
+
+    qsort(stack_scores.items, stack_scores.count, sizeof(uint64_t), vector_sort_comp);
+
+    printf("%ld\n", stack_scores.items[stack_scores.count / 2]);
     
     free(line);    
-
     free(stack.values);
-
-    printf("%d\n", stack_error_value);
+    free(stack_scores.items);
 
     return 0;
+}
+
+void vector_push_back(vector_t * v, uint64_t value) {
+    v->items[v->count++] = value;
+    if(v->count == v-> capacity) {
+        v->capacity *= 2;
+        v->items = reallocarray(v->items, v->capacity, sizeof(uint64_t));
+    }
+}   
+
+int vector_sort_comp(const void * a, const void * b) {
+    if (*(uint64_t *)a > *(uint64_t *)b) {
+        return 1;
+    } else if (*(uint64_t *)a < *(uint64_t *)b) {
+        return -1;
+    } else {
+        return 0;
+    }
+    
 }
